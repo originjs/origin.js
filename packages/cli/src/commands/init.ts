114cef26d5efad3973dea1bbd9d9b8c065c6f1cf
Janson1012 <1402225856@qq.com>
2021-07-28 19:02:31 +0800

    refactor: change code location

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,43 +1,40 @@
 import fs from 'fs-extra';
+import ora from 'ora';
 const download = require('download-git-repo')
-const ora =require('ora')
-export function init(name: any, options:any) {
-       // 1.Check whether there is a folder with the same name as the project name in the current folder
-       // and whether the project name is legal
-        const createDirIfNotExists = (dir:any) => {
-           if(!fs.existsSync(dir) ){
-              const reg = /[< > / \ | : " * ?]/g;
-              if(reg.test(name)){
-                  console.error('ProjectName is illegal')
-                  return false
-              }
-            }else {
-                console.error('Dir already exists')
-                return false
-            }
-        };
-        createDirIfNotExists(name);
-        // 2. Pull template from github to new project
-        const combineFiles= async (name:any) => {
-          const spinner = ora('Downloading...')
-          spinner.start()
-              try {
-                await new Promise<void>((resolve, reject) => {
-                  download('wangAlisa/vue-temp', name,{ clone:false },(error:any)=>{
-                    if(error) {
-                      spinner.fail()
-                      return reject(error)
-                    }
-                    spinner.succeed()
-                    resolve()
-                  })
-                })
 
-              } catch (error) {
-                console.error('Failed to initialize the template')
-                console.log(error)
-                return false
+const createDirIfNotExists = (dir:any) => {
+    // Check whether there is a folder with the same name as the project name in the current folder
+    // and whether the project name is legal
+        if(!fs.existsSync(dir) ){
+           const reg = /[< > / \ | : " * ?]/g;
+           if(reg.test(name)){
+               console.error('ProjectName is illegal')
+               return false
+           }
+         }else {
+             console.error('Dir already exists')
+             return false
+         }
+};
+export async function init(name: any, options:any) {
+        createDirIfNotExists(name);
+        //Pull template from github to new project
+        const spinner = ora('Downloading...')
+        spinner.start()
+        try {
+          await new Promise<void>((resolve, reject) => {
+            download('wangAlisa/vue-temp', name,{ clone:false },(error:any)=>{
+              if(error) {
+                spinner.fail()
+                return reject(error)
               }
+              spinner.succeed()
+              resolve()
+            })
+          })
+        } catch (error) {
+          console.error('Failed to initialize the template')
+          console.log(error)
+          return false
         }
-        combineFiles(name)
 }
