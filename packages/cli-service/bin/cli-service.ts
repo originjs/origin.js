8a941bf9c08a8c597c7989f499a94fd6f7584f1a
mulinJiao <joemullin@qq.com>
2021-08-05 10:01:33 +0800

    feat(cli): change bin/*.js to ts.

---
+++ packages/cli-service/bin/cli-service.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { run } from '../src'
+import { argv } from 'process'
+
+const mode: string = argv[2]
+if (mode === 'dev' || mode === 'build') {
+  run(mode).catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
+}
