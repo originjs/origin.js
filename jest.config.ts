b5c755e69357c9fa24900406369d69b114175028
wangsongc <wangsongc@foxmail.com>
2021-08-17 11:50:14 +0800

    test: add test coverage (#28)

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,14 @@ const config: Config.InitialOptions = {
   testMatch: ['<rootDir>/packages/**/__tests__/**/*.spec.[jt]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   transform: {
-    '\\.(ts|tsx)$':'ts-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
   },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'lcov', 'text'],
+  collectCoverageFrom: [
+    'packages/*/src/**/*.ts',
+    '!packages/runtime-test/src/utils/**',
+  ],
 }
 
 export default config
