757e433e7f94d7067f4db944795e8f7eadf022fb
PeterAlfredLee <peteralfredlee@gmail.com>
2021-08-26 15:28:42 +0800

    chore: update last commit

--- packages/vite-plugin-pages/src/generates.ts
+++ packages/vite-plugin-pages/src/generates.ts
@@ -1,4 +1,4 @@
-// import { getLayoutProperties, setLayout } from './parser'
+import { getLayoutProperties, setLayout } from './parser'
 import { PluginOptions, Route, Page } from './types'
 
 /**
@@ -44,7 +44,7 @@ export function generateRoutes(
       name: '',
       path: '',
       component: `${page.pathFromRootDir}`,
-      // meta: { layout: getLayoutProperties(pathFromPagesDir) },
+      meta: { layout: getLayoutProperties(pathFromPagesDir) },
     }
 
     let parentRoutes = routes
@@ -95,7 +95,7 @@ export function generateRoutes(
   }
 
   // set layouts to routes
-  // setLayout(routes, options)
+  setLayout(routes, options)
 
   return routes
 }
