b6ccbd37657df97780ef24bb6c6077bdc05ea8f6
Alan Wang <948467222@qq.com>
2021-09-03 10:58:26 +0800

    fix: add transform hook to avoid layout block error (#51)

---
+++ packages/vite-plugin-pages/examples/vue3/src/layouts/profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="border:1px solid grey">
+    <h1>profile layout</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+}
+</script>
+
