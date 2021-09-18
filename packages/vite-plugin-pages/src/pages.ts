72bc3905cba26e833ce8259067243a4cdbe0a29f
chenshuming <csmdev@hotmail.com>
2021-09-18 09:58:55 +0800

    feat(vite-plugin-pages): move sort to pages.ts

--- packages/vite-plugin-pages/src/pages.ts
+++ packages/vite-plugin-pages/src/pages.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
 import { Page } from './types'
-import { getFiles, sortFilePaths } from './files'
+import { getFiles } from './files'
 import { replaceWithSlash } from './utils'
 
 function getPage(pathFromPagesDir: string, pathFromRootDir: string): Page {
@@ -10,9 +10,40 @@ function getPage(pathFromPagesDir: string, pathFromRootDir: string): Page {
   }
 }
 
+/**
+ * sort pages by pathFromPagesDir:
+ * 1. treat xx, _xx, _$xx as same name
+ * 2. in each directory: index.vue is the first, _.vue is the last
+ */
+export function sortPages(pages: Page[]): Page[] {
+  function normalStr(str: string): string {
+    if (!str.startsWith('/')) {
+      str = `/${str}`
+    }
+    return str
+      .replace('index.vue', '') // remove 'index.vue'
+      .replace('_.vue', '~.vue') // '_.vue' => '~.vue'
+      .replace(/\/_/g, '/') // '/_' => '/'
+      .replace(/\/\$/g, '/') // '/$' => '/'
+  }
+
+  pages.sort((a, b) => {
+    const strA = normalStr(a.pathFromPagesDir)
+    const strB = normalStr(b.pathFromPagesDir)
+    if (strA < strB) {
+      return -1
+    }
+    if (strA > strB) {
+      return 1
+    }
+    return 0
+  })
+  return pages
+}
+
 export function getPages(pagesDir: string, extensions: string[]): Page[] {
-  const files = sortFilePaths(getFiles(pagesDir, extensions))
-  return files.map(file =>
-    getPage(file, replaceWithSlash(join(pagesDir, file))),
+  const files = getFiles(pagesDir, extensions)
+  return sortPages(
+    files.map(file => getPage(file, replaceWithSlash(join(pagesDir, file)))),
   )
 }
