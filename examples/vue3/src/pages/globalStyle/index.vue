32b494b3a12b5d2be7c02be3480406398b07a9aa
PeterAlfredLee <peteralfredlee@gmail.com>
2021-09-23 17:12:14 +0800

    feat: add global style in example

---
+++ examples/vue3/src/pages/globalStyle/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <a id="scss_test">The color of this part is influenced by global scss</a>
+    <br>
+    <a id="less_test">The color of this part is influenced by global less</a>
+    <br>
+    <a id="stylus_test">The color of this part is influenced by global stylus</a>
+  </div>
+</template>
+
+<script>
+import '@/assets/test.scss'
+import '@/assets/test.less'
+import '@/assets/test.styl'
+
+export default {
+  name: 'GlobalStyle',
+}
+</script>
+
+<style scoped>
+
+</style>
+
+<layout>
+layout: content
+</layout>
--- examples/vue3/vite.config.ts
+++ examples/vue3/vite.config.ts
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import pluginAssets from '../../packages/vite-plugin-global-style/src/index'
 import pluginContent from '../../packages/vite-plugin-content/src/index'
 import pluginPages from '../../packages/vite-plugin-pages/src/index'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), pluginAssets(), pluginContent(), pluginPages()],
+  resolve: {
+    alias : {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
 })
