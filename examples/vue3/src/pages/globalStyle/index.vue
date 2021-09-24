76138bc85ffa41b7172f5c3260c5d28d6b2af27d
PeterAlfredLee <peteralfredlee@gmail.com>
2021-09-24 09:43:58 +0800

    feat: update global style in example

--- examples/vue3/src/pages/globalStyle/index.vue
+++ examples/vue3/src/pages/globalStyle/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <a id="scss_test">The color of this part is influenced by global scss</a>
+    <a id="scss_test">The style of this part is influenced by global sass</a>
     <br>
-    <a id="less_test">The color of this part is influenced by global less</a>
+    <a id="less_test">The style of this part is influenced by global less</a>
     <br>
-    <a id="stylus_test">The color of this part is influenced by global stylus</a>
+    <a id="stylus_test">The style of this part is influenced by global stylus</a>
   </div>
 </template>
 
