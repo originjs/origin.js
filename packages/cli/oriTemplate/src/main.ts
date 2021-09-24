2c53219b4212edfc7ee730e637f4d91bea3540ec
konpeki622 <512054675@qq.com>
2021-09-24 16:15:24 +0800

    fix: components plugin imports

--- packages/cli/oriTemplate/src/main.ts
+++ packages/cli/oriTemplate/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'plugin-pages'
+import routes from 'virtual:plugin-pages'
 
 const router = createRouter({
   history: createWebHistory(),
