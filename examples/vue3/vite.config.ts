b954d008dcb816d68cd1253cc62af9f9b1cc09d3
Alanscut <wp_scut@163.com>
2021-09-28 10:22:05 +0800

    fix: alias path changed to ./scr

--- examples/vue3/vite.config.ts
+++ examples/vue3/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [vue(), pluginAssets(), pluginContent(), pluginPages()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '/src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
