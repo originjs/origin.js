716fb188af528bd4a04e0197f7ce39da6141b955
konpeki622 <512054675@qq.com>
2022-03-29 17:59:28 +0800

    fix: line feed

--- packages/cli/__tests__/cli.spec.ts
+++ packages/cli/__tests__/cli.spec.ts
@@ -409,10 +409,10 @@ test('ori tovue3', async () => {
   const { exitCode, stdout } = await run(['tovue3', '--help'])
   expect(stdout).toMatchSnapshot('A8')
   expect(exitCode).toEqual(0)
-}, 10000)
+}, 15000)
 
 test('ori tovite', async () => {
   const { exitCode, stdout } = await run(['tovite', '--help'])
   expect(stdout).toMatchSnapshot('A9')
   expect(exitCode).toEqual(0)
-}, 10000)
+}, 15000)
